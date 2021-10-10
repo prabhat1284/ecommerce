d84bcb35e4f82a33c95f72168ca4b02e677b839f
SAI ALEKYA <sai-alekya.k@capgemini.com>
2021-10-10 12:37:31 +0530

    product page added

--- src/components/products/Dynamicprod.js
+++ src/components/products/Dynamicprod.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Cartbtn from "./Cartbtn";
@@ -18,25 +19,15 @@ function Dynamicprod() {
       {prodDetails.map((s) => (
         <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12">
           <div className="product-box">
-            <i>
-              {/* eslint-disable-next-line jsx-a11y/alt-text*/}
-              <img src="../public/product/noimg.png" />
-            </i>
-            <table>
-              <td>
-                {/* eslint-disable-next-line jsx-a11y/alt-text*/}
-                <img
-                  src="../public/product/{s.prodimg}"
-                  width="50%"
-                  height="50%"
-                />
-              </td>
-              <td>{s.pname}</td>
-              <td>Rs.{s.price}</td>
-              <br />
-              <br />
-              <Cartbtn ptitle={s.pname} price={s.price} />
-            </table>
+            <ul>
+              <li>{s.cname}</li>
+              <li>{s.pname}</li>
+              <li>Rs.{s.price}</li>
+              <li>
+                <img src="../../../public/prodimages/1632463368869.png" />
+              </li>
+            </ul>
+            <Cartbtn ptitle={s.pname} price={s.price} />
           </div>
         </div>
       ))}
@@ -45,3 +36,30 @@ function Dynamicprod() {
 }
 
 export default Dynamicprod;
+
+{
+  /* <table class="table">
+              <tr width="100%" heigth="100%">
+                <td>{s._id}</td>
+                <td>
+                  <b>{s.cname}</b>
+                </td>
+                <td>{s.pname}</td>
+                <td>
+                  {/*eslint-disable-next-line jsx-a11y/alt-text*/
+}
+{
+  /* <img src="../../../public/prodimages/{s.prodimg}" width="50%" heigth="50%" />
+                </td> */
+}
+{
+  /* <td>{s.pdescription}</td>
+                <td>Rs.{s.price}</td>
+                <td> */
+}
+{
+  /* <Cartbtn ptitle={s.pname} price={s.price} />
+                </td>
+              </tr>
+            </table>  */
+}
